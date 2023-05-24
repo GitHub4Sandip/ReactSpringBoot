c1e2a0aba1375bc0f28ef9279172f4b81a8e0739
unknown <samantaray.sandip@gmail.com>
2023-05-24 23:30:30 +0530

    Initial Commit

---
+++ src/main/java/com/reactspringboot/bookStore/controller/BookController.java
@@ -0,0 +1,26 @@
+package com.reactspringboot.bookStore.controller;
+
+import com.reactspringboot.bookStore.dto.BookDto;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1")
+public class BookController {
+
+  @GetMapping("books")
+  public ResponseEntity<List<BookDto>> getBooks() {
+    BookDto book = BookDto.builder()
+        .title("My first book title")
+        .build();
+    List<BookDto> books = new ArrayList<>();
+    books.add(book);
+    return ResponseEntity.ok(books);
+  }
+
+}
